d7847dbac179701d0142472e437e90767c720e9d
cameronfrost08 <87400853+cameronfrost08@users.noreply.github.com>
2021-09-28 17:20:50 -0500

    Add files via upload

---
+++ Bryan-university/week1/week2/my-Website.js
@@ -0,0 +1,37 @@
+var ListItem=document.createElement('li')
+var myList=document.getElementById('My-list')
+myList.appendChild(ListItem)
+ListItem.textContent='0'
+ListItem.textContent='1'
+ListItem.textContent='2'
+
+var newHeader=document.createElement('h1')
+newHeader.textContent='Welcome to my JS site'
+document.body.appendChild(newHeader)
+
+var newP = document.createElement('p')
+
+newP.textContent= 'All of this was created in Javascript'
+document.body.appendChild(newP)
+
+var a = document.createElement("a")
+var linkText= document.createTextNode("this is the title")
+a.appendChild("https://www.google.com/","https://www.youtube.com/")
+a.title= ("here are sonme links")
+document.body.appendChild(a)
+
+var f = document.createElement("footer")
+var p= document.createElement("p")
+var aHref= document.createElement("a")
+var Href= document.createAttribute("href")
+
+p.textContent=("Name: John Doe")
+aHref.textContent=("johndoe@example.com")
+Href.textContent=("johndoe@example.com")
+f.append.body(footer)
+p.appendChild.footer(p)
+a.appendChild.footer(a)
+
+
+
+
